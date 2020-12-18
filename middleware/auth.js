8f8ac99d862bf46a1c3ea696a83158699792336d
Naitchi <assasindu39@gmail.com>
2020-12-18 14:00:18 +0100

    delete a useless console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(process.env.PASSWORD_SECRET_TOKEN);
     const decodedToken = jwt.verify(token, process.env.PASSWORD_SECRET_TOKEN);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
